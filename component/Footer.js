79b0804af3a12b44c7619eeae202f7ce37632a09
Ashis Kumar <37765033+ashiskumar-1999@users.noreply.github.com>
2021-02-01 01:22:25 +0530

    FooterSection, SocialMediaSection has been added along with it's Responsive design.

--- component/Footer.js
+++ component/Footer.js
@@ -1,11 +1,43 @@
-import { Box, Flex } from "@chakra-ui/react"
 import React from "react"
+import { Text, Icon, Box, VStack, HStack } from "@chakra-ui/react"
+import { FaFacebook, FaInstagram, FaGithub } from "react-icons/fa"
 
 const Footer = () => {
   return (
-    <Flex>
-      <Box></Box>
-    </Flex>
+    <VStack
+      spacing={3}
+      py="30px"
+      px="30px"
+      justifyContent="center"
+      alignContent="center"
+      bg="#000000"
+    >
+      <Text
+        fontFamily="Rubik"
+        fontSize={["lg", "lg", "2xl", "2xl"]}
+        fontWeight="normal"
+        textAlign="center"
+        letterSpacing="wide"
+        color="#ffffff"
+      >
+        You never know what you could possibly build at Hackathons.
+      </Text>
+      <Text
+        fontFamily="Rubik"
+        fontSize={["xl", "xl", "4xl", "4xl"]}
+        fontWeight="bold"
+        textAlign="center"
+        letterSpacing="wide"
+        color="#ffffff"
+      >
+        Come hack with us!
+      </Text>
+      <HStack spacing={4}>
+        <Icon as={FaFacebook} boxSize="2em" color="#ffffff" />
+        <Icon as={FaInstagram} boxSize="2em" color="#ffffff" />
+        <Icon as={FaGithub} boxSize="2em" color="#ffffff" />
+      </HStack>
+    </VStack>
   )
 }
 
---
+++ component/SocialMediaSection.js
@@ -0,0 +1,76 @@
+import React from "react"
+import { Flex, Box, Icon, Button, Text, VStack, Stack } from "@chakra-ui/react"
+import { FaDiscord, FaTwitter } from "react-icons/fa"
+
+const Socialmedia = () => {
+  return (
+    <Stack
+      spacing={4}
+      direction={{ base: "column", md: "row" }}
+      bg="#7289DA"
+      py={["30px", "30px", "40px", "50px"]}
+      px="30px"
+      justifyContent="space-around"
+      alignItems="center"
+    >
+      <VStack spacing={4} justifyContent="center" alignItems="center">
+        <Icon
+          as={FaDiscord}
+          color="#ffffff"
+          boxSize={["4em", "4em", "5em", "6em"]}
+        />
+        <Text
+          textAlign="center"
+          fontFamily="Rubik"
+          fontSize={["lg", "xl", "2xl", "3xl"]}
+          fontWeight="bold"
+          letterSpacing="wide"
+          lineHeight="shorter"
+          color="#ffffff"
+        >
+          Start the Conversation
+        </Text>
+        <Button
+          size={["lg"]}
+          color="#ffffff"
+          borderRadius="0px"
+          outline="none"
+          _hover={{ bg: "gray.300" }}
+          bg="#000000"
+        >
+          Join our Discord
+        </Button>
+      </VStack>
+      <VStack spacing={4} justifyContent="center" alignItems="center">
+        <Icon
+          as={FaTwitter}
+          boxSize={["4em", "4em", "5em", "6em"]}
+          color="#ffffff"
+        />
+        <Text
+          textAlign="center"
+          fontFamily="Rubik"
+          fontSize={["lg", "xl", "2xl", "3xl"]}
+          fontWeight="bold"
+          letterSpacing="wide"
+          lineHeight="shorter"
+          color="#ffffff"
+        >
+          Don't miss any updates!
+        </Text>
+        <Button
+          size={["lg"]}
+          color="#ffffff"
+          borderRadius="0px"
+          outline="none"
+          _hover={{ bg: "gray.300" }}
+          bg="#000000"
+        >
+          Follow us on Twitter
+        </Button>
+      </VStack>
+    </Stack>
+  )
+}
+
+export default Socialmedia
