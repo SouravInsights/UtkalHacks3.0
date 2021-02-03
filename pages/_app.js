2d437f6190655f5eb483147ecf18338283c548f1
Ashis Kumar <37765033+ashiskumar-1999@users.noreply.github.com>
2021-02-03 22:07:03 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import React from "react"
 import { ChakraProvider, theme } from "@chakra-ui/react"
+import Fonts from "../styles/Fonts"
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
+      <Fonts />
       <Component {...pageProps} />
     </ChakraProvider>
   )
