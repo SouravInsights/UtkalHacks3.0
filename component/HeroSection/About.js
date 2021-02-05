37d8c95c63c303843499b39a85960daa9e837f70
catchsahil <sahilsahilkumar1234@gmail.com>
2021-02-05 23:34:52 +0530

    Update About.js

--- component/HeroSection/About.js
+++ component/HeroSection/About.js
@@ -22,20 +22,20 @@ const About = () => {
         <Aboutcard
           coverImage="https://res.cloudinary.com/emishalabs/image/upload/q_30/v1612203030/UtkalHacks%203.0/camputer-image_zmpt7b.webp"
           header="A better working environment"
-          body="No more chaotic working table. No more random snacks on the floor. Now you can just lock yourself in your room and get the work done! "
+          body="No more chaotic working table. No more random snacks on the floor. Now you can just lock yourself in your room and get the work done!"
         />
 
         <Aboutcard
           coverImage="https://res.cloudinary.com/emishalabs/image/upload/q_30/v1612203738/UtkalHacks%203.0/E122FB34-4451-46B4-880A-A992B5EDD8AF_p2okwy.webp"
           header="Better physical & mental state"
-          body=" No more back strains. No more burnouts. Now you can get into serious hacking without serious repercussions, focusing on your idea and not that sleeping space you could miss out on."
+          body="No more late night struggles. No more burnouts. Now you can get into serious hacking without serious repercussions, focusing on your idea and not that sleeping space you could miss out on."
         />
 
         <Aboutcard
           coverImage="https://res.cloudinary.com/emishalabs/image/upload/q_30/v1612203007/UtkalHacks%203.0/8530A1AB-D8AB-4A67-8081-8ACCBA272448_buopz2.webp"
           header="Better team collaboration"
           body="
-          No more limitations on teammate inclusiveness. No more distractions. Now you can get anyone from anywhere on your team and collaborate in your own solitary virtual space. "
+          No more random sticky notes. No more flip chats. Now you can get anyone from anywhere on your team and collaborate effectively in your own solitary virtual space."
         />
       </Stack>
     </Box>
