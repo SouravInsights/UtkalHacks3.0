57312a25b10e07e27b5adffc412d449d1c4a7aa3
Ashis Kumar <37765033+ashiskumar-1999@users.noreply.github.com>
2021-02-06 03:02:16 +0530

    PrizeCard and PrizeSection added. All links added.

--- component/Button.js
+++ component/Button.js
@@ -1,39 +1,36 @@
-import React from 'react'
+import React from "react"
 import { Box, HStack, Spacer } from "@chakra-ui/react"
 
-const Button = ({ label, rightIcon, withIcon, ...props }) => {
-    return (
-        <HStack
-            spacing="16px"
-            as="button"
-            outline="none"
-            lineHeight="1"
-            letterSpacing="wide"
-            transition="all 0.2s cubic-bezier(.08,.52,.52,1)"
-            py="18px"
-            px="34px"
-            borderRadius="2px"
-            fontSize="16px"
-            fontWeight="semibold"
-            bg="#1D1B1B"
-            borderColor="#ccd0d5"
-            color="#ffffff"
-            _hover={{ bg: "#ffffff", color: "#1D1B1B" }}
-            _active={{
-                bg: "#ffffff",
-                transform: "scale(0.98)",
-                borderColor: "#1D1B1B",
-            }}
-
-            {...props}
-        >
-            <Box textAlign="center">
-                {label}
-            </Box>
-            {withIcon ? <Box> {rightIcon} </Box> : null}
-
-        </HStack>
-    )
+const Button = ({ label, rightIcon, withIcon, onClick, ...props }) => {
+  return (
+    <HStack
+      spacing="16px"
+      as="button"
+      outline="none"
+      lineHeight="1"
+      letterSpacing="wide"
+      transition="all 0.2s cubic-bezier(.08,.52,.52,1)"
+      py="18px"
+      px="34px"
+      borderRadius="2px"
+      fontSize="16px"
+      fontWeight="semibold"
+      bg="#1D1B1B"
+      borderColor="#ccd0d5"
+      color="#ffffff"
+      _hover={{ bg: "#ffffff", color: "#1D1B1B" }}
+      _active={{
+        bg: "#ffffff",
+        transform: "scale(0.98)",
+        borderColor: "#1D1B1B",
+      }}
+      onClick={onClick}
+      {...props}
+    >
+      <Box textAlign="center">{label}</Box>
+      {withIcon ? <Box> {rightIcon} </Box> : null}
+    </HStack>
+  )
 }
 
 export default Button
---
+++ component/Card/PrizeCard.js
@@ -0,0 +1,20 @@
+import React from "react"
+import { Heading, Text, Image, VStack, Link, Flex, Box } from "@chakra-ui/react"
+
+function PrizeCard({ CoverImage, heading, description, to }) {
+  return (
+    <Link href={to} isExternal>
+      <VStack minW="300px" maxW="350px" p="16px" border="1px solid #252222">
+        <Image src={CoverImage} />
+        <Heading fontSize="24px" fontWeight="bold" textAlign="center" py="16px">
+          {heading}
+        </Heading>
+        <Text fontSize="lg" fontWeight="normal" textAlign="justify">
+          {description}
+        </Text>
+      </VStack>
+    </Link>
+  )
+}
+
+export default PrizeCard
