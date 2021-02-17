3edcca0abfb91663726fdfa3827aa9fb290428d4
Ashis Kumar <37765033+ashiskumar-1999@users.noreply.github.com>
2021-02-17 12:24:49 +0530

    Prizes has been added.

--- component/Card/PrizeCard.js
+++ component/Card/PrizeCard.js
@@ -1,10 +1,27 @@
 import React from "react"
-import { Heading, Text, Image, VStack, Link, Flex, Box } from "@chakra-ui/react"
+import {
+  Heading,
+  Text,
+  Image,
+  VStack,
+  Link,
+  List,
+  ListItem,
+  ListIcon,
+} from "@chakra-ui/react"
+import { MdCheckCircle } from "react-icons/md"
 
-function PrizeCard({ CoverImage, heading, description, to }) {
+const PrizeCard = ({ CoverImage, heading, description, data = [], to }) => {
   return (
     <Link href={to} _hover={{ outline: "none" }} isExternal>
-      <VStack minW="300px" maxW="350px" p="16px" border="1px solid #252222">
+      <VStack
+        minW="300px"
+        maxW="350px"
+        minH="380px"
+        p="16px"
+        border="1px solid #252222"
+        my="20px"
+      >
         <Image src={CoverImage} />
         <Heading fontSize="24px" fontWeight="bold" textAlign="center" py="16px">
           {heading}
@@ -12,6 +29,14 @@ function PrizeCard({ CoverImage, heading, description, to }) {
         <Text fontSize="lg" fontWeight="normal" textAlign="justify">
           {description}
         </Text>
+        <List spacing={3}>
+          {data.map((prizeData) => (
+            <ListItem key={prizeData} color="#000000">
+              <ListIcon as={MdCheckCircle} color="black.500" />
+              {prizeData}
+            </ListItem>
+          ))}
+        </List>
       </VStack>
     </Link>
   )
